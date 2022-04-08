47aa5c0cf8bb1d4df5af028666fa2db60cd8eceb
sfstoevring <sftoevring@gmail.com>
2022-04-08 13:50:18 +0200

    new

---
+++ src/FxScene.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+
+
+
+public class FxScene extends Application{
+    public void start(Stage stage) throws Exception {
+        Label label = new Label("label her");
+        HBox hBox = new HBox();
+
+        Scene scene = new Scene(hBox,600, 400);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
