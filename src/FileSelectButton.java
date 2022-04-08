cec1838066114d6647537afd5e7cf0095646dff4
Jblomp <jblomp@ruc.dk>
2022-04-08 15:29:50 +0200

    Making UI

---
+++ src/FileSelectButton.java
@@ -0,0 +1,19 @@
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+class FileSelectButton extends Button {
+    FileSelectButton(String dir) {
+        setText("Press");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(dir));
+        setOnAction(e -> {
+            File file = fileChooser.showOpenDialog(new Stage());
+            String fileName = file.getAbsolutePath();
+            System.out.println(fileName);
+        });
+    }
+    //EncryptFileECB encryptor = new EncryptFileECB();
+}
--- src/FxScene.java
+++ src/FxScene.java
@@ -1,20 +1,45 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 
 
 public class FxScene extends Application{
     public void start(Stage stage) throws Exception {
-        Label label = new Label("label her");
-        HBox hBox = new HBox(label);
 
-        Scene scene = new Scene(hBox,600, 400);
+        stage.setTitle("File Encrypter");
+        //Dirctory in project for Crytography
+        String dir = "Files4Project";
+        Label label = new Label("Cryptography");
+        label.setUnderline(true);
 
+        ToggleGroup toggleGroup = new ToggleGroup();
+        RadioButton radioButton = new RadioButton("Encrypt");
+        RadioButton radioButton1 = new RadioButton("Decrypt");
+        radioButton.setToggleGroup(toggleGroup);
+        radioButton.setSelected(true);
+        radioButton1.setToggleGroup(toggleGroup);
+
+        FileSelectButton button = new FileSelectButton(dir);
+
+
+        HBox hBox = new HBox(radioButton,radioButton1);
+        hBox.setAlignment(Pos.BASELINE_CENTER);
+        hBox.setSpacing(25);
+
+
+        VBox vBox = new VBox(label, hBox,button);
+        vBox.setAlignment(Pos.BASELINE_CENTER);
+        vBox.setSpacing(50);
+        Scene scene = new Scene(vBox, 500, 400);
+        scene.getStylesheets().add("file:style.css");
         stage.setScene(scene);
         stage.show();
     }
+
 }
