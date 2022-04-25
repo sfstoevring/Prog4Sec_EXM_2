9e9e51b691ffac2d98df0edd5b30a29cbfcf6f01
Jblomp <jblomp@ruc.dk>
2022-04-25 12:31:40 +0200

    Helping the old thinkpad

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        //--module-path C:\Users\JesperBlom\Documents\GitHub\Prog4Sec_EXM_2\JavaAddOns\JavaFXDir\javafx-sdk-17.0.2\lib --add-modules javafx.controls
 
     }
 }
